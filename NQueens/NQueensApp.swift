4824e9ea52fae49517243b089d73a3f934e06a0d
Vladimir Klevtsov <zonda4242@gmail.com>
2026-02-19 12:55:22 +0100

    Initial Commit

---
+++ NQueens/NQueensApp.swift
@@ -0,0 +1,17 @@
+//
+//  NQueensApp.swift
+//  NQueens
+//
+//  Created by Владимир Клевцов on 19. 2. 2026..
+//
+
+import SwiftUI
+
+@main
+struct NQueensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
